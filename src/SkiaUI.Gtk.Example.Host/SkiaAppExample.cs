c22313254448ebfa556058109cb8a442caa33358
Guy Langston <guylangston@gmail.com>
2026-03-27 09:52:27 +0000

    misc

--- src/SkiaUI.Gtk.Example.Host/SkiaAppExample.cs
+++ src/SkiaUI.Gtk.Example.Host/SkiaAppExample.cs
@@ -10,9 +10,10 @@ public class SkiaAppExample : ISkiaApp
     public int FrameCount { get; set; }
     public Func<object, object> HostCallback { get; set; }
 
-    private SKFont fontDefault;
-    private SKPaint paintBlack;
-    private SKPaint paintWhite;
+    private readonly SKFont fontDefault;
+    private readonly SKPaint paintBlack;
+    private readonly SKPaint paintWhite;
+    int eventCounter=0;
 
     public SkiaAppExample()
     {
@@ -40,17 +41,16 @@ public class SkiaAppExample : ISkiaApp
         return true;
     }
 
-    int cc=0;
 
     public void HandleKeyPress(SkiaAppKey key)
     {
-        Console.WriteLine($"{cc++}:HandleKeyPress {key.Key}");
+        Console.WriteLine($"{eventCounter++}:HandleKeyPress {key.Key}");
         if (key.Key == "q") SendHost("Quit");
     }
 
     public void HandleMousePress(SkiaAppMouse mouse)
     {
-        Console.WriteLine($"{cc++}:HandleMousePress X:{mouse.X}, Y:{mouse.Y} Btn:{mouse.Button}[{mouse.Type}]");
+        Console.WriteLine($"{eventCounter++}:HandleMousePress X:{mouse.X}, Y:{mouse.Y} Btn:{mouse.Button}[{mouse.Type}]");
     }
 
     public void Paint(SKSurface surface)
